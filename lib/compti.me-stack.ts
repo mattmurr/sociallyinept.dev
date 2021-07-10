d14dd8c88ba86c83a73d235246fb0c05e367e1e6
Matthew Murray <mattmurr.uk@gmail.com>
2021-07-10 13:29:11 +0100

    Simple S3 hosting

--- lib/compti.me-stack.ts
+++ lib/compti.me-stack.ts
@@ -1,9 +1,26 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as s3deploy from "@aws-cdk/aws-s3-deployment";
 
 export class ComptiMeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "Bucket", {
+      bucketName: "compti.me",
+      websiteIndexDocument: "index.html",
+      publicReadAccess: true,
+      // Delete the bucket when running cdk destroy
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    new s3deploy.BucketDeployment(this, "DeployWithInvalidation", {
+      sources: [s3deploy.Source.asset("./site/public")],
+      destinationBucket: bucket,
+      // Don't retain objects in the bucket
+      retainOnDelete: false,
+    });
+
+    new cdk.CfnOutput(this, "BucketURL", { value: bucket.bucketWebsiteUrl });
   }
 }
