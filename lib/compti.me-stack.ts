b7f42ccedfb74d071ddb185f79d5503c097c1ad6
Matthew Murray <mattmurr.uk@gmail.com>
2021-07-14 18:47:29 +0100

    Use cloudfront.Distribution
    Automagically sets up the Origin Access Identity for the S3 bucket

--- lib/compti.me-stack.ts
+++ lib/compti.me-stack.ts
@@ -2,10 +2,10 @@ import * as cdk from "@aws-cdk/core";
 import * as s3 from "@aws-cdk/aws-s3";
 import * as s3deploy from "@aws-cdk/aws-s3-deployment";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
+import * as origins from "@aws-cdk/aws-cloudfront-origins";
 import * as acm from "@aws-cdk/aws-certificatemanager";
 import * as route53 from "@aws-cdk/aws-route53";
 import * as route53targets from "@aws-cdk/aws-route53-targets";
-import * as iam from "@aws-cdk/aws-iam";
 
 export interface ComptiMeProps extends cdk.StackProps {
   domainName: string;
@@ -18,10 +18,7 @@ export class ComptiMeStack extends cdk.Stack {
     const domainName = props.domainName;
 
     const bucket = new s3.Bucket(this, "Bucket", {
-      bucketName: "compti.me",
-      websiteIndexDocument: "index.html",
-      //publicReadAccess: true,
-
+      bucketName: props.domainName,
       // Delete the bucket when running cdk destroy
       removalPolicy: cdk.RemovalPolicy.DESTROY,
 
@@ -34,30 +31,7 @@ export class ComptiMeStack extends cdk.Stack {
     // Find a reference to my domain's hosted zone
     const zone = route53.HostedZone.fromLookup(this, "Zone", { domainName });
 
-    // Restrict access to the S3 content
-    // See: https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/private-content-restricting-access-to-s3.html
-    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
-      this,
-      "cloudfront-OAI",
-      {
-        comment: `OAI for ${id}`,
-      }
-    );
-
     new cdk.CfnOutput(this, "SiteURL", { value: "https://" + domainName });
-
-    // Explicitly Allow cloudfront to access the S3 bucket
-    bucket.addToResourcePolicy(
-      new iam.PolicyStatement({
-        actions: ["s3:GetObject"],
-        resources: [bucket.arnForObjects("*")],
-        principals: [
-          new iam.CanonicalUserPrincipal(
-            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
-          ),
-        ],
-      })
-    );
 
     const certificate = new acm.DnsValidatedCertificate(
       this,
@@ -71,12 +45,6 @@ export class ComptiMeStack extends cdk.Stack {
       }
     );
 
-    const certificateArn = certificate.certificateArn;
-
-    new cdk.CfnOutput(this, "Certificate", {
-      value: certificateArn,
-    });
-
     const urlRewriteFunction = new cloudfront.Function(
       this,
       "UrlRewriteIndexHtmlFunction",
@@ -89,40 +57,21 @@ export class ComptiMeStack extends cdk.Stack {
       }
     );
 
-    const distribution = new cloudfront.CloudFrontWebDistribution(
-      this,
-      "SiteDistribution",
-      {
-        aliasConfiguration: {
-          acmCertRef: certificateArn,
-          names: [domainName],
-          sslMethod: cloudfront.SSLMethod.SNI,
-          securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_1_2016,
-        },
-        originConfigs: [
+    const distribution = new cloudfront.Distribution(this, "SiteDistribution", {
+      defaultBehavior: {
+        origin: new origins.S3Origin(bucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        functionAssociations: [
           {
-            s3OriginSource: {
-              s3BucketSource: bucket,
-              originAccessIdentity: cloudfrontOAI,
-            },
-            behaviors: [
-              {
-                isDefaultBehavior: true,
-                compress: true,
-                allowedMethods:
-                  cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
-                functionAssociations: [
-                  {
-                    function: urlRewriteFunction,
-                    eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
-                  },
-                ],
-              },
-            ],
+            function: urlRewriteFunction,
+            eventType: cloudfront.FunctionEventType.VIEWER_REQUEST,
           },
         ],
-      }
-    );
+      },
+      defaultRootObject: "index.html",
+      domainNames: [domainName],
+      certificate: certificate,
+    });
 
     new cdk.CfnOutput(this, "DistributionDomainName", {
       value: distribution.domainName,
@@ -131,8 +80,6 @@ export class ComptiMeStack extends cdk.Stack {
     new s3deploy.BucketDeployment(this, "DeployWithInvalidation", {
       sources: [s3deploy.Source.asset("./site/output")],
       destinationBucket: bucket,
-      // Don't retain objects in the bucket
-      retainOnDelete: false,
 
       distribution,
       distributionPaths: ["/*"],
