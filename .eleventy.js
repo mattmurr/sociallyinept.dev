964977bed7e38e9bdf9cc51a65924e49d33c061a
Matthew Murray <mattmurr.uk@gmail.com>
2021-08-07 14:56:27 +0100

    JSON RSS feed
    See jsonfeed.org for specs

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const Image = require("@11ty/eleventy-img");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 async function imageShortcode(src, alt) {
   const sizes = "(min-width: 1024px), 100vw, 50vw";
@@ -28,6 +29,7 @@ async function imageShortcode(src, alt) {
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginRss);
 
   eleventyConfig.addFilter("postDate", (date) =>
     DateTime.fromJSDate(date).toLocaleString(DateTime.DATE_MED)
