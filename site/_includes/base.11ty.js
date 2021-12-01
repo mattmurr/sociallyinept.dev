b5ac5b59ae2c73bbf82878fa189d093630c93c2c
Matthew Murray <matt@thick.rocks>
2021-12-01 21:15:11 +0000

    favicon

--- site/_includes/base.11ty.js
+++ site/_includes/base.11ty.js
@@ -12,6 +12,10 @@ exports.render = function ({ site, content, page, title, description }) {
           content="${description || site.desc}"
         />
         <link rel="canonical" href=${site.url + page.url} />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets//apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets//favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets//favicon-16x16.png">
+        <link rel="manifest" href="/assets//site.webmanifest">
         <link href="/assets/main.css" rel="stylesheet"/>
       </head>
       <body>
