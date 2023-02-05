a9e78a985b84c87576a78950e7a549722299fb0f
Matthew Murray <elegant.dusk3770@fastmail.com>
2023-02-05 21:03:15 +0000

    remove redirect_url

--- api/auth/github.ts
+++ api/auth/github.ts
@@ -3,16 +3,14 @@ export const config = {
   regions: ["lhr1"],
 };
 
-
 // Redirect to GitHub auth
 export default (req: Request) => {
   const AUTH_URL = "https://github.com/login/oauth/authorize";
-  const REDIRECT_PATH = "/api/auth/callback/github";
 
   return new Response(null, {
     status: 302,
     headers: {
-      Location: `${AUTH_URL}?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=https://${process.env.VERCEL_URL}${REDIRECT_PATH}`,
+      Location: `${AUTH_URL}?client_id=${process.env.GITHUB_CLIENT_ID}`,
     },
   });
 };
