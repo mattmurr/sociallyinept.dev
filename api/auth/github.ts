dfc5ace23717f28662af085eb15ef5ec6a372fd5
Matthew Murray <elegant.dusk3770@fastmail.com>
2023-02-05 20:55:57 +0000

    Or this

--- api/auth/github.ts
+++ api/auth/github.ts
@@ -9,18 +9,10 @@ export default (req: Request) => {
   const AUTH_URL = "https://github.com/login/oauth/authorize";
   const REDIRECT_PATH = "/api/auth/callback/github";
 
-  const CLIENT_ID_KEY = "GITHUB_CLIENT_ID";
-
-  if (!process.env.hasOwnProperty(CLIENT_ID_KEY)) {
-    throw new Error(
-      "GITHUB_CLIENT_ID environment variable is required."
-    );
-  }
-
   return new Response(null, {
     status: 302,
     headers: {
-      Location: `${AUTH_URL}?client_id=${process.env[CLIENT_ID_KEY]}&redirect_uri=${process.env.URL}${REDIRECT_PATH}`,
+      Location: `${AUTH_URL}?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${process.env.URL}${REDIRECT_PATH}`,
     },
   });
 };
